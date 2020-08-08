a5d49f2f69be8983f3fe87c60c0706baf74d57bb
kripsonud <kripsonud@gmail.com>
2020-08-08 09:45:00 +0100

    Add Trigger

--- Assets/Scripts/GameObjects/Weapons/Trigger.cs
+++ Assets/Scripts/GameObjects/Weapons/Trigger.cs
@@ -9,6 +9,27 @@ namespace Assets.Scripts.GameObjects.Weapons
 {
     public class Trigger : MonoBehaviour
     {
+        private void Update()
+        {
+            if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+            {
+                Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                RaycastHit raycastHit;
+                if (Physics.Raycast(raycast, out raycastHit))
+                {
+                    Debug.Log("Something Hit");
+                    if (raycastHit.collider.name == "Trigger")
+                    {
+                        Debug.Log("Soccer Ball clicked");
+                    }
 
+                    //OR with Tag
+                    if (raycastHit.collider.CompareTag("Trigger"))
+                    {
+                        Debug.Log("Soccer Ball clicked");
+                    }
+                }
+            }
+        }
     }
 }
