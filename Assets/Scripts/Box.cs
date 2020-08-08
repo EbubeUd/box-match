faf5da06ebcdfb4a7b4969146133d79680f13fc8
GHOST\GHOST <marshalonah@gmail.com>
2020-08-08 12:48:17 +0100

    Boxes updated

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public SpriteRenderer SpriteRenderer;
+    public Sprite Sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer.sprite = Sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
