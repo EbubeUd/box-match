b3b0644c468efbf5bb2772cf0a9df18c77dd2d0e
Precious Kosisochukwu <kcokolo10@gmail.com>
2020-08-29 15:55:46 +0100

    Home Sceen

---
+++ Assets/Scripts/Management/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Management
+{
+    public class UIManager : MonoBehaviour
+    {
+        public Button PlayButton;
+
+        // Renders the sample screen
+        public void Play()
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
