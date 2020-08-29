3fcc2dc7d1a02f1814188fdd42f7529d93bfcdf8
GHOST\GHOST <marshalonah@gmail.com>
2020-08-29 11:40:44 +0100

    Game Manager script

---
+++ Assets/Scripts/Management/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    [HideInInspector]
+    public bool IsGamePaused;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    void PauseGame(bool status)
+    {
+        IsGamePaused = status;
+    }
+
+}
