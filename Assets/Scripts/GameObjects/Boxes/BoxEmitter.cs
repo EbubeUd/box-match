c76a36bb542c43da0a214d4ddec496b281f53d05
GHOST\GHOST <marshalonah@gmail.com>
2020-08-29 10:51:19 +0100

    BoxEmitter Script

---
+++ Assets/Scripts/GameObjects/Boxes/BoxEmitter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxEmitter : MonoBehaviour
+{
+    public GameObject BoxPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Started");
+        InvokeRepeating("SpawnBox", 2f, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnBox()
+    {
+        Instantiate(BoxPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
